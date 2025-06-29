1ccb3e6579d3cd04e412ef5e1edd7548722beab2
wasiftak <wtak732@gmail.com>
2025-06-29 14:18:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from textblob import TextBlob
+from newspaper import Article
+
+with open('text.txt', 'r') as file:
+    text = file.read()
+    
+# url = 'https://www.bbc.com/news/articles/cze10y59j91o'
+# article = Article(url)
+
+# article.download()
+# article.parse()
+# article.nlp()
+
+# text = article.summary
+# print(text)
+
+blob = TextBlob(text)
+sentiment=blob.sentiment.polarity
+print(sentiment)
